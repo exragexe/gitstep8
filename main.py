c90e83c72dffa2ce283d424a5dfeeff3a4d8d23a
yaros <yaroslavdisiak@gmail.com>
2022-11-06 00:50:18 +0200

    hm 1 done

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 
+
+try:
+
+    def viraz():
+        user = input("Введіть арифметичний вираз. Приклад(12 + 7): ")
+
+        user_sp = user.split(" ")
+        res = [float(i) for i in user_sp[0::2]]
+
+        if len(user_sp) > 3:
+            return "Error. Вираз може складатися тільки з трьох частин: число, операція, число"
+        elif "+" in user_sp:
+            return f"Сума виразу дорівнює - {res[0] + res[1]}"
+        elif "-" in user_sp:
+            return f"Різниця виразу дорівнює - {res[0] - res[1]}"
+        elif "*" in user_sp:
+            return f"Добуток виразу дорівнює - {res[0] * res[1]}"
+        elif "/" in user_sp:
+            return f"Ділення виразу дорівнює - {res[0] / res[1]}"
+        else:
+            return "Error. Можливі операції: +, -, *, /."
+
+    print(viraz())
+
+
+except Exception as ex:
+    print(ex)
