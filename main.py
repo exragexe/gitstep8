f67a2c2f2b8e0d045447a9a26d52f130e5cdf510
yaros <yaroslavdisiak@gmail.com>
2022-11-05 22:59:09 +0200

    lab 3 done

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+
+try:
+    num = input("Введіть список чисел: ")
+    num_sp = num.split(" ")
+    res = [float(i) for i in num_sp]
+
+
+    print(f"Сумма чисел = {float(sum(res))} \n AVG = {float(sum(res) / len(num_sp))}")
+except Exception as e:
+    print(e)
