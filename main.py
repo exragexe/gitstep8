e93a8f1c46818ed06fe21d7cdb1e6d09fe9f34cd
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-11-05 13:42:45 +0200

    lab 2 done

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+
+
+try:
+    numbers = input("Ведіть список чисел:")
+    fnum = input("Введіть число яке ви шукаєте:")
+    num = 0
+
+    if fnum not in numbers:
+        print(f"{fnum} not in text")
+
+    res = numbers.count(fnum)
+
+
+
+    print(f"Кількість {fnum} в тексті - {res}")
+
+except Exception as e:
+    print(e)
